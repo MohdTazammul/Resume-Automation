5c3e4a079f8f8fc312e8d66dd4bd65e0814f0ed0
Mohd Tazammul <tazammul1683@gmail.com>
2022-04-25 02:27:37 +0530

    Form Completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
@@ -6,6 +7,31 @@ function App() {
   return (
       <Form /> 
        );
+=======
+import ResponsiveAppBar from "./components/navbar";
+// import DownloadResume from "./components/DownloadResume/downloadResume";
+import Form from "./components/Form";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DownloadResume from "./components/downloadResume";
+import "./App.css";
+
+
+function App() {
+  return (
+  <BrowserRouter>
+    <div className="App">
+    <ResponsiveAppBar />
+     <Routes>
+     <Route path="/home" element={<DownloadResume />}>
+     </Route>  
+     
+     <Route path="/form" element={<Form />}>
+     </Route>
+     </Routes>
+    </div>
+  </BrowserRouter>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App;
