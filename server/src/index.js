96a928c339e644f008c94895052da6c3aa8ff29d
anchor-abhi <theabhishek1802@gmail.com>
2022-05-01 00:03:02 +0530

    server updated

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const connect = require("../src/configs/db");
 require("dotenv").config();
 const PORT = process.env.PORT || 4567;
 const app = express();
+const path=require("path")
 const ResumeController = require("./controllers/resume.controller");
 const UserController = require("./controllers/user.controller")
 
@@ -10,6 +11,7 @@ var cors = require('cors')
 app.use(cors());
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, ".public")));
 
 app.use("/resume", ResumeController);
 app.use("/user", UserController);
