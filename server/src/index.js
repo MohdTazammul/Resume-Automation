b9ccbfabc697699b7e8b4989f33fe6cfdcf7ffe7
anchor-abhi <theabhishek1802@gmail.com>
2022-05-01 00:36:05 +0530

    path updated

--- server/src/index.js
+++ server/src/index.js
@@ -11,8 +11,8 @@ var cors = require('cors')
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../")));
-
+app.use(express.static(path.join(__dirname, "../../public")));
+console.log(path.join(__dirname, "../../public"));
 app.use("/resume", ResumeController);
 app.use("/user", UserController);
 
