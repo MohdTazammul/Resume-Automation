07709f4bc2535f5fc23510e7a78f6c4206d242c2
anchor-abhi <theabhishek1802@gmail.com>
2022-05-01 15:25:58 +0530

    started deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connect = require("../src/configs/db");
 require("dotenv").config();
-const PORT = process.env.PORT || 4567;
+const PORT = process.env.PORT || 4444;
 const app = express();
 const path=require("path")
 const ResumeController = require("./controllers/resume.controller");
