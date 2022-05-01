83a19b6c596ead3f16664e7b3b15c82868b067d6
anchor-abhi <theabhishek1802@gmail.com>
2022-05-01 15:49:35 +0530

    start changed

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../../")));
 
-console.log(path.join(__dirname, "../../public"));
+
 app.use("/resume", ResumeController);
 app.use("/user", UserController);
 
