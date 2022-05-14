95c85edd932c903a3e26c2cf8b847753dd0d0058
anchor-abhi <theabhishek1802@gmail.com>
2022-05-14 17:10:27 +0530

    removed non used files

--- src/components/userAuth.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-// import SignIn from "./components/signin";
-import SignUp from "./signup";
-
-export default function UserAuth(){
-    const [logged, setLog] = useState(false);
-    return <SignUp/>
-}
