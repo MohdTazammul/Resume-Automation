afd6a4748cc516881c054b409b2b97a5f83cb38e
anchor-abhi <theabhishek1802@gmail.com>
2022-05-15 15:45:35 +0530

    update pass added

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,158 +1,179 @@
 // import {send_email} from "./aws-utils";
 // send_email = require("./aws_utils");
+const nodemailer = require("nodemailer")
 const express = require("express");
 const User = require("../models/user.model");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 var crypto = require("crypto");
-var AWS = require('aws-sdk');
-
+const bcrypt = require("bcryptjs");
 require("dotenv").config();
-const {body, validationResult} = require("express-validator");
 
-const newToken=(user)=>{
-    return jwt.sign({user}, `${process.env.MY_KEY}`)
+function verifyToken(token){
+    return new Promise((resolve, reject)=>{
+        jwt.verify(token, `${process.env.MY_KEY}`, (err, user)=>{
+            if(err){
+                return reject(err);
+            }
+
+            resolve(user);
+        })
+    })
 }
 
-router.get("", async(req, res)=>{
+
+const { body, validationResult } = require("express-validator");
+
+const newToken = (user) => {
+    
+  return jwt.sign({ user }, `${process.env.MY_KEY}`);
+};
+
+router.get("", async (req, res) => {
+  try {
+    const user = await User.find().lean().exec();
+    res.send(user);
+  } catch (e) {
+    res.send(e.message);
+  }
+});
+
+router.post(
+  "/register",
+  body("firstName").isString().isLength({ min: 3, max: 20 }),
+  body("lastName").isString().isLength({ min: 3, max: 20 }),
+  body("email")
+    .isEmail()
+    .withMessage("Enter correct email format")
+    .custom(async (value) => {
+      const user = await User.find({ email: value }).lean().exec();
+      console.log(user);
+      if (user.length > 0) {
+        throw new Error("Email already exist, try another");
+      }
+      return true;
+    }),
+  body("password")
+    .isStrongPassword()
+    .withMessage(
+      "Password must contain uppercase and lowercase character, number and special symbols, password must be 8 characters long"
+    ),
+  async (req, res) => {
+    try {
+      let errors = validationResult(req);
+      if (errors.errors.length > 0) {
+        return res.send(errors);
+      }
+
+      let user = await User.find({ email: req.body.email });
+
+      if (user.length > 0) {
+        return res.send("User already exists, try another email");
+      }
+
+      user = await User.create(req.body);
+      let token = newToken(user);
+      res.send({ user, token });
+    } catch (e) {
+      res.send(e.message);
+    }
+  }
+);
+
+router.post("/update-password/:token", async(req, res)=>{
     try{
-        const user = await User.find().lean().exec();
-        res.send(user);
+        const user = await verifyToken(req.params.token);
+        console.log(user)
+        var hash = bcrypt.hashSync(req.body.password, 8);
+        const thisUser = await User.findOneAndUpdate(
+            { email: user.user.email },
+            { password: hash },
+            { new: true }
+          );
+            return res.send(thisUser);
     }
     catch(e){
         res.send(e.message);
     }
-});
+})
 
-router.post("/register",
-body("firstName").isString().isLength({min:3, max:20}),
-body("lastName").isString().isLength({min:3, max:20}),
-body("email").isEmail().withMessage("Enter correct email format").custom(async (value)=>{
-    const user =await User.find({email:value}).lean().exec();
-    console.log(user)
-    if(user.length>0){
-        throw new Error('Email already exist, try another')
-    }
-    return true;
-}),
-body("password").isStrongPassword().withMessage("Password must contain uppercase and lowercase character, number and special symbols, password must be 8 characters long")
-,
-async(req, res)=>{
-    try{
-        let errors = validationResult(req);
-        if(errors.errors.length>0){
-            return res.send(errors);
-        }
-        
-        let user = await User.find({email:req.body.email});
-        
-        if(user.length>0){
-            return res.send("User already exists, try another email");
-        }
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.find({ email: req.body.email });
 
-        user = await User.create(req.body);
-        let token = newToken(user);
-        res.send({user, token});
-        
+    if (user.length == 0) {
+      return res.send({
+        err: "User does not exist, Please try creating an account",
+      });
     }
-    catch(e){
-        res.send(e.message);
+    if (user[0].checkPassword(req.body.password)) {
+      let token = newToken(user);
+      res.send({ user, token });
+    } else {
+      res.send({ err: "Email or password not correct" });
     }
-});
-
-router.post("/login", async(req, res)=>{
-    try{
-        const user = await User.find({email:req.body.email});
-
-        if(user.length==0){
-            
-            return res.send({err:"User does not exist, Please try creating an account"});
-        }
-        if(user[0].checkPassword(req.body.password)){
-            
-            let token = newToken(user);
-            res.send({user, token});
-        }
-        else{
-            res.send({err:"Email or password not correct"});
-        }
-    }
-    catch(e){
-        res.send(e.message);
-    }
+  } catch (e) {
+    res.send(e.message);
+  }
 });
 
 
-router.post("/forgot-password", async(req, res)=>{
-    try{
-        const user = await User.find({email:req.body.email});
-
-        if(user.length===0){
-            return res.send({err:"User does not exist, Please try creating an account"});
-        }
-        if(user[0].email){
-            var random_password = crypto.randomBytes(6).toString('hex');
-
-            User.updateOne({email: req.body.email}, 
-                {
-                    $set: {
-                        password: random_password,
-                    }
-                } )
-            send_email(req.body.email, random_password);
-            res.send("Please check your email for new password!")
-        }
-        else{
-            res.send({err:"Email not available!"});
-        }
+router.post("/forgot-password", async (req, res) => {
+  try {
+    
+    const user = await User.find({ email: req.body.email });
+    
+    if (user.length === 0) {
+      return res.send({
+        err: "User does not exist, Please try creating an account",
+      });
     }
-    catch(e){
-        res.send(e.message);
+    if (user[0].email) {
+        let token = newToken(user[0]);
+    //   var random_password = crypto.randomBytes(6).toString("hex");
+    //   var hash = bcrypt.hashSync(random_password, 8);
+
+    //   const thisUser = await User.findOneAndUpdate(
+    //     { email: req.body.email },
+    //     { password: hash },
+    //     { new: true }
+    //   );
+
+      send_email(req.body.email, token);
+      return res.send("Email sent successfully");
+    } else {
+      res.send({ err: "Email not available!" });
     }
+  } catch (e) {
+      console.log(e)
+    res.send(e.message);
+  }
 });
 
-function send_email(email, password){
-    AWS.config.update({region: 'ap-south-1'});
-
-    var params = {
-    Destination: { /* required */
-        ToAddresses: [
-        email,
-        ]
-    },
-    Message: { /* required */
-        Body: { /* required */
-        Html: {
-        Charset: "UTF-8",
-        Data: "Your new password is " + password
-        },
-        Text: {
-        Charset: "UTF-8",
-        Data: "Your new password is " + password,
+function send_email(email, token) {
+    
+    let mailTransporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: `${process.env.SOURCE}`,
+            pass: `${process.env.PASS}`
         }
-        },
-        Subject: {
-        Charset: 'UTF-8',
-        Data: 'Password Reset'
-        }
-        },
-    Source: 'lohit@masaischool.com',
-    };
-
-    // Create the promise and SES service object
-    var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-
-    // Handle promise's fulfilled/rejected states
-    sendPromise.then(
-    function(data) {
-        console.log(data.MessageId);
-        return;
-    }).catch(
-        function(err) {
-        console.error(err, err.stack);
-        return;
     });
+    
+    let mailDetails = {
+        from: 'abhishek.maurya@masaischool.com',
+        to: email,
+        subject: 'Password Update',
+        text: `https://tempresume.vercel.app/forgot-password/${token}`
+    };
+    
+    mailTransporter.sendMail(mailDetails, function(err, data) {
+        if(err) {
+            console.log('Error Occurs');
+        } else {
+            console.log('Email sent successfully');
+        }
+    });
+  };  
 
-}
-
-module.exports=router;
+module.exports = router;
