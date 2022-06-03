810434f1f650d1ef585f902f608a1f5290ac14de
anchor-abhi <theabhishek1802@gmail.com>
2022-06-03 14:05:25 +0530

    link-check added in post resume

--- server/src/controllers/resume.controller.js
+++ server/src/controllers/resume.controller.js
@@ -1,39 +1,87 @@
 const express = require("express");
+const linkCheck = require("link-check");
 const Resume = require("../models/resume.model");
 
-const router  = express.Router();
+const router = express.Router();
 
-router.post("", async(req, res)=>{
-    try{
-        const resume = await Resume.create(req.body);
-        console.log(resume);
-        res.send(resume);
+const linkCheckAsync = (param) => {
+  return new Promise((res, rej) => {
+    linkCheck(param, function (err, result) {
+      if (err) rej(false);
+      res(result.statusCode);
+    });
+  });
+};
+
+router.post("", async (req, res) => {
+  try {
+    let obj = {};
+    let flag = true;
+    obj.portfolio = [req.body.personal.portfolio, true];
+    obj.github = [req.body.personal.github, true];
+    obj.projects = [];
+    req.body.projects.forEach((element) => {
+      obj.projects.push({
+        liveLink: [element.liveLink, true],
+        gitLink: [element.gitLink, true],
+      });
+    });
+
+    const res1 = await linkCheckAsync(obj["portfolio"][0]);
+    if (res1 !== 200) {
+      obj["portfolio"][1] = false;
+      flag = false;
     }
-    catch(e){
-        console.log(e.message)
-        res.send(e.message);
+    const res2 = await linkCheckAsync(obj["github"][0]);
+    if (res2 !== 200) {
+      obj["github"][1] = false;
+      flag = false;
     }
+
+    let i = 0;
+    for (const elem of obj.projects) {
+      let urlLive = elem.liveLink[0];
+      let urlGit = elem.gitLink[0];
+
+      let res = await linkCheckAsync(urlLive);
+      if (res !== 200) obj.projects[i].liveLink[1] = false;
+
+      let res2 = await linkCheckAsync(urlGit);
+      if (res2 !== 200) obj.projects[i].gitLink[1] = false;
+      i++;
+    }
+
+    if (!flag) {
+      console.log(obj);
+      return res.send({err:"Wrong Link",obj});
+    } else {
+      const resume = await Resume.create(req.body);
+      console.log(resume);
+      return res.send(resume);
+    }
+
+  } catch (e) {
+    console.log(e.message);
+    res.send(e.message);
+  }
 });
 
-router.get("", async(req, res)=>{
-    try{
-        const resume = await Resume.find().lean().exec();
-        res.send(resume);
-    }
-    catch(e){
-        res.send(e.message);
-    }
+router.get("", async (req, res) => {
+  try {
+    const resume = await Resume.find().lean().exec();
+    res.send(resume);
+  } catch (e) {
+    res.send(e.message);
+  }
 });
 
-router.get("/:id", async(req, res)=>{
-    try{
-        const resume = await Resume.find({user:req.params.id}).lean().exec();
-        res.send(resume);
-    }
-    catch(e){
-        res.send(e.message);
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const resume = await Resume.find({ user: req.params.id }).lean().exec();
+    res.send(resume);
+  } catch (e) {
+    res.send(e.message);
+  }
 });
-
 
 module.exports = router;
--- server/src/index.js
+++ server/src/index.js
@@ -3,31 +3,29 @@ const connect = require("../src/configs/db");
 require("dotenv").config();
 const PORT = process.env.PORT || 4444;
 const app = express();
-const path=require("path")
+const path = require("path");
 const ResumeController = require("./controllers/resume.controller");
-const UserController = require("./controllers/user.controller")
+const UserController = require("./controllers/user.controller");
 
-var cors = require('cors')
+var cors = require("cors");
 app.use(cors());
 
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../../")));
 
-
 app.use("/resume", ResumeController);
 app.use("/user", UserController);
 
 app.get("/", (req, res) => {
-    try {
-      res.send("Home")
-    } catch (e) {
-      res.send(e.message);
-    }
-  });
+  try {
+    res.send("Home");
+  } catch (e) {
+    res.send(e.message);
+  }
+});
 
-
-app.listen(PORT, ()=>{
-    connect();
-    console.log(`Listening at ${PORT}`);
-})
+app.listen(PORT, () => {
+  connect();
+  console.log(`Listening at ${PORT}`);
+});
